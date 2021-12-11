1d3059b457ed08ff21b6323a4e79aa37f2f5bb12
NincsJobbOtletem <55430954+NincsJobbOtletem@users.noreply.github.com>
2021-12-11 23:27:35 +0100

    First commit

---
+++ backend/src/main/resources/db/migration/V2__insert_employees_table.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO employees (first_name, last_name, email, job_title, phone, image_url) VALUES
+('Hazle', 'Reilly', 'songokuvagyok@gmail.com', 'Junior Backend Developer', '249-515-0345', 'https://i.pinimg.com/564x/b4/48/59/b448594cc21bcb1347fd3626988e1dfb.jpg'),
+('Pedro', 'Prado', 'Pedro__prado@hotmail.com', 'F1 driver', '+34 913 56 4991', 'https://i.pinimg.com/originals/be/3f/bd/be3fbda09e5a391c9575eee85e93c8a1.png'),
+('Arnau', 'Ayala', 'ignacio.hernando@unideb.hu', 'Linux', '+34 906094616', 'https://i.pinimg.com/originals/c7/d8/82/c7d882749bb5e5e78e20854a632f05ab.jpg'),
+('Yuki ', 'Thunoda', 'sayuri57@matsumoto.com', 'php', '0040-813-446', 'https://i.pinimg.com/564x/d1/9c/8e/d19c8ef676be37384f00489c21939db3.jpg');
